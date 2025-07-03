cb9204e33b1b682fc82d8cc9bf73cd8af7ccdcf8
miguel007TMM2 <climaxfran@gmail.com>
2025-07-03 16:58:15 -0400

    details

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as express from "express"
+import { Request, Response } from "express";
 import * as bodyParser from "body-parser"
 import { AppDataSource } from "./data-source"
 import { index } from "./controllers/index";
@@ -8,11 +9,27 @@ AppDataSource.initialize().then(async () => {
 
     const app = express()
     const corsOptions = {
-      origin: ["http://localhost:3000"],
+      origin: ["http://localhost:3000", "http://localhost:3800"],
       credentials: true,
     };
+    
+    app.use((req: Request, res: Response, next: Function) => {
+      const timestamp = new Date().toISOString();
+      console.log(`[${timestamp}] ${req.method} ${req.url}`);
+      console.log('Headers:', req.headers);
+      if (req.body && Object.keys(req.body).length > 0) {
+        console.log('Body:', req.body);
+      }
+      if (req.query && Object.keys(req.query).length > 0) {
+        console.log('Query params:', req.query);
+      }
+      console.log('---');
+      next();
+    });
+    
     app.use(bodyParser.json())
     app.use(cors(corsOptions))
+
     app.use("/api", index);
     app.listen(process.env.PORT || 3800)
     console.log("Express server has started on port 3000. Open http://localhost:3800 to see results")
